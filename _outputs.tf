86fb13a604e6a2d22d4779cb2439bd14c06492b4
Renato <renato@dnx.solutions>
2023-02-20 16:03:25 +1100

    fix output

--- _outputs.tf
+++ _outputs.tf
@@ -1,29 +1,65 @@
 output "transit_gateway_id" {
-  value       = aws_ec2_transit_gateway.default[0].id
+  value = var.transit_gateway_enabled ? aws_ec2_transit_gateway.default.*.id : null
   description = "Transit Gateway ID"
 }
 
-output "transit_gateway_route_table_private_id" {
-  value       = aws_route.private.route_table_id
-  description = "Transit Gateway route table private ID"
+output "transit_gateway_default_route_table_id" {
+  value = var.transit_gateway_enabled ? aws_ec2_transit_gateway.default.*.association_default_route_table_id : null
+  description = "Transit Gateway route table id default"
 }
 
-output "transit_gateway_route_table_public_id" {
-  value       = aws_route.public.route_table_id
-  description = "Transit Gateway route table public ID"
+output "transit_gateway_account_owner_id" {
+  value = var.transit_gateway_enabled ? aws_ec2_transit_gateway.default.*.owner_id : null
+  description = "Transit Gateway AWS account ID owner"
 }
 
-output "transit_gateway_route_table_secure_id" {
-  value       = aws_route.secure.route_table_id
-  description = "Transit Gateway route table secure ID"
+output "transit_gateway_cidr_blocks" {
+  value = var.transit_gateway_enabled ? aws_ec2_transit_gateway.default.*.transit_gateway_cidr_blocks : null
+  description = "Transit Gateway CIDR blocks"
 }
 
-output "transit_gateway_attachment_id" {
-  value       = aws_ec2_transit_gateway_route.blackhole.transit_gateway_attachment_id
-  description = "Transit Gateway route table secure ID"
+output "transit_attachment_id" {
+  value       =  var.attachment ? aws_ec2_transit_gateway_vpc_attachment.default.*.id : null
+  description = "Transit Gateway attachment"
 }
 
-output "transit_gateway_id" {
-  value       = aws_ec2_transit_gateway_vpc_attachment.default.transit_gateway_id
-  description = "Transit Gateway Default ID"
+output "transit_attachment_account_owner_id" {
+  value       =  var.attachment ? aws_ec2_transit_gateway_vpc_attachment.default.*.vpc_owner_id : null
+  description = "Transit Gateway attachment AWS Account owner"
+}
+
+output "public_nacls" {
+  value = {
+    "acl" : var.public_network_acl_id
+     "egress" : {
+      "public_out_requester_to_accepter" : length(var.public_network_acl_id) > 0 ? aws_network_acl_rule.public_out_requester_to_accepter : {}
+    }
+    "ingress" : {
+      "public_in_requester_from_accepter" : length(var.public_network_acl_id) > 0 ? aws_network_acl_rule.public_in_requester_from_accepter : {}
+    }
+  }
+}
+
+output "private_nacls" {
+  value = {
+    "acl" : var.private_network_acl_id
+    "egress" : {
+      "private_out_requester_from_accepter" : length(var.private_network_acl_id) > 0 ? aws_network_acl_rule.private_out_requester_to_accepter : {}
+    }
+    "ingress" : {
+      "private_in_requester_from_accepter" : length(var.private_network_acl_id) > 0 ? aws_network_acl_rule.private_in_requester_from_accepter : {}
+    }
+  }
+}
+
+output "secure_nacls" {
+  value = {
+    "acl" : var.secure_network_acl_id
+     "egress" : {
+      "secure_out_requester_from_accepter" : length(var.secure_network_acl_id) > 0 ? aws_network_acl_rule.secure_out_requester_to_accepter : {}
+    }
+    "ingress" : {
+      "secure_in_requester_from_accepter" : length(var.secure_network_acl_id) > 0 ? aws_network_acl_rule.secure_out_requester_to_accepter : {}
+    }
+  }
 }
