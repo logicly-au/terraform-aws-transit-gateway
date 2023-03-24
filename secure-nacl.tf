6d135cfaf140082e8bdfe16f64f4b2c0fb33b590
Renato <renato@dnx.solutions>
2023-03-24 16:55:49 +1100

    add ephemeral ports variable option

--- secure-nacl.tf
+++ secure-nacl.tf
@@ -29,3 +29,35 @@ resource "aws_network_acl_rule" "secure_out_requester_to_accepter" {
   from_port      = each.value.port
   to_port        = each.value.port == 0 ? "65535" : each.value.port
 }
+
+resource "aws_network_acl_rule" "secure_in_requester_ephemeral" {
+ for_each = {
+    for network in var.secure_route : network.cidr => network
+    if try(network.nacl_inbound_ephemeral_ports, false) == true
+  }
+
+  network_acl_id = var.secure_network_acl_id
+  rule_number    = 2000 + index(var.secure_route, each.value)
+  egress         = false
+  protocol       = "tcp"
+  rule_action    = "allow"
+  cidr_block     = each.value.cidr
+  from_port      = 1024
+  to_port        = 65535
+}
+
+resource "aws_network_acl_rule" "secure_out_requester_ephemeral" {
+ for_each = {
+    for network in var.secure_route : network.cidr => network
+    if try(network.nacl_outbound_ephemeral_ports, false) == true
+  }
+
+  network_acl_id = var.secure_network_acl_id
+  rule_number    = 2000 + index(var.secure_route, each.value)
+  egress         = true
+  protocol       = "tcp"
+  rule_action    = "allow"
+  cidr_block     = each.value.cidr
+  from_port      = 1024
+  to_port        = 65535
+}
